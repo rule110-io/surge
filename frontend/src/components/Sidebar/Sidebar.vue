bba058dde1558547c1a64eb2ca39c80e4f720dc8
Andrey <andrtaw1@gmail.com>
2020-07-11 18:47:28 +0300

    feat: add feather icons, add nav

--- frontend/src/components/Sidebar/Sidebar.vue
+++ frontend/src/components/Sidebar/Sidebar.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="sidebar">
-    <router-link to="/foo" class="sidebar__logo" />
+    <div class="sidebar__logo" />
+    <div class="sidebar__nav">
+      <router-link to="/download" class="sidebar__item">
+        <feather class="sidebar__item-icon" type="play"></feather
+      ></router-link>
+      <router-link to="/folder" class="sidebar__item">
+        <feather class="sidebar__item-icon" type="file"></feather>
+      </router-link>
+      <router-link to="/trash" class="sidebar__item">
+        <feather class="sidebar__item-icon" type="trash"></feather
+      ></router-link>
+    </div>
+    <router-link to="/settings" class="sidebar__item">
+      <feather class="sidebar__item-icon" type="settings"></feather
+    ></router-link>
   </div>
 </template>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,15 @@ import "regenerator-runtime/runtime";
 import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
+import VueFeather from "vue-feather";
+
 import * as Wails from "@wailsapp/runtime";
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
 Vue.use(VueRouter);
+Vue.use(VueFeather);
 
 const Foo = { template: "<div>foo</div>" };
 const Bar = { template: "<div>bar</div>" };
