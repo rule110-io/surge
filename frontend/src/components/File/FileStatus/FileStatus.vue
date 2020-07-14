69322d41fda8e9be62ff8b873aeb8ea35350e2f9
Andrey <andrtaw1@gmail.com>
2020-07-14 18:28:23 +0300

    feat: add fileStatus, fileTime components

---
+++ frontend/src/components/File/FileStatus/FileStatus.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="file-status">
+    <div class="file-status__speed">34.5 MB/s</div>
+    <div class="file-status__progress">
+      <div
+        class="file-status__progress-current"
+        :style="{ width: `${progress}%` }"
+      ></div>
+      <div class="file-status__progress-default"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./FileStatus.scss";
+</style>
+
+<script>
+export default {
+  props: {
+    file: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      progress: 70,
+    };
+  },
+  methods: {},
+};
+</script>
---
+++ frontend/src/components/File/FileTime/FileTime.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="file-time">
+    <div class="file-time__title">11 min</div>
+    <div class="file-time__percent">80%</div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./FileTime.scss";
+</style>
+
+<script>
+export default {
+  props: {
+    file: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
