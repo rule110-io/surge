dc56426a50f201cd995ec0f196d565a0625a1e72
Andrey <andrtaw1@gmail.com>
2020-07-22 01:20:09 +0300

    feat: add chunkmap preogress bar

--- frontend/src/components/File/FileStatus/FileStatus.vue
+++ frontend/src/components/File/FileStatus/FileStatus.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="file-status">
     <div class="file-status__speed">{{ bandwidth | prettyBytes(1) }}/s</div>
-    <div class="file-status__progress">
-      <div
-        class="file-status__progress-current"
-        :style="{ width: `${progress}%` }"
-      ></div>
-      <div class="file-status__progress-default"></div>
-    </div>
+    <canvas
+      class="file-status__progress"
+      ref="canvas"
+      width="400"
+      height="6"
+    ></canvas>
   </div>
 </template>
 
@@ -27,7 +26,6 @@ export default {
   },
   data() {
     return {
-      progress: 0,
       bandwidth: 0,
     };
   },
@@ -36,16 +34,39 @@ export default {
   },
   watch: {
     downloadEvent(newEvent) {
-      console.log(newEvent);
-      if (this.file.FileHash === newEvent.FileHash) {
+      const { FileHash } = this.file;
+      if (FileHash === newEvent.FileHash) {
         this.bandwidth = newEvent.Bandwidth;
-        this.progress = newEvent.Progress * 100;
+        this.drawProgress(newEvent.ChunkMap);
       }
     },
   },
   mounted() {
-    this.progress = this.file.IsUploading && !this.file.IsDownloading ? 100 : 0;
+    this.getChunkMap();
   },
-  methods: {},
+  methods: {
+    getChunkMap() {
+      window.backend.getFileChunkMap(this.file.FileHash, 400).then((bits) => {
+        this.drawProgress(bits);
+      });
+    },
+    drawProgress(bits) {
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext("2d");
+      const colours = ["#ebebeb", "#02d2b3"];
+
+      const bitmap = `${bits}`.split("");
+
+      bitmap.forEach((val, i) => {
+        ctx.beginPath();
+        ctx.strokeStyle = colours[parseFloat(val)];
+        ctx.lineWidth = 1;
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, 6);
+        ctx.closePath();
+        ctx.stroke();
+      });
+    },
+  },
 };
 </script>
