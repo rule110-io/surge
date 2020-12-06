b715760b065ac429ad0ada062a060ce142b7851c
Mitchel Disveld <mitchel.disveld@hotmail.com>
2020-12-06 21:54:49 +0100

    60s for all timeouts

--- surge/SurgeCore.go
+++ surge/SurgeCore.go
@@ -882,7 +882,7 @@ func restartDownload(Hash string) {
 				chunkInTransitLock.Unlock()
 
 				//Sleep for 30 seconds, check if entry still exists in transit map.
-				time.Sleep(time.Second * 180)
+				time.Sleep(time.Second * 60)
 				inTransit := chunksInTransit[chunkKey]
 
 				//If its still in transit abort
