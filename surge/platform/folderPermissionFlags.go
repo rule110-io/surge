505d45fcb20f3f0a07e6976a817320c464118622
Mitchel Disveld <mitchel.disveld@hotmail.com>
2020-12-14 19:59:17 +0100

    moved permissions to shared file in platform package

---
+++ surge/platform/folderPermissionFlags.go
@@ -0,0 +1,35 @@
+package platform
+
+//OS folder permission bitflags
+const (
+	osRead       = 04
+	osWrite      = 02
+	osEx         = 01
+	osUserShift  = 6
+	osGroupShift = 3
+	osOthShift   = 0
+
+	osUserR   = osRead << osUserShift
+	osUserW   = osWrite << osUserShift
+	osUserX   = osEx << osUserShift
+	osUserRw  = osUserR | osUserW
+	osUserRwx = osUserRw | osUserX
+
+	osGroupR   = osRead << osGroupShift
+	osGroupW   = osWrite << osGroupShift
+	osGroupX   = osEx << osGroupShift
+	osGroupRw  = osGroupR | osGroupW
+	osGroupRwx = osGroupRw | osGroupX
+
+	osOthR   = osRead << osOthShift
+	osOthW   = osWrite << osOthShift
+	osOthX   = osEx << osOthShift
+	osOthRw  = osOthR | osOthW
+	osOthRwx = osOthRw | osOthX
+
+	osAllR   = osUserR | osGroupR | osOthR
+	osAllW   = osUserW | osGroupW | osOthW
+	osAllX   = osUserX | osGroupX | osOthX
+	osAllRw  = osAllR | osAllW
+	osAllRwx = osAllRw | osGroupX
+)
