afadcfa03c2d41335ddebc34e3b6d8db243501c1
lightmyfire <andrtaw1@gmail.com>
2021-03-27 20:39:13 +0300

    feat: add tabs

---
+++ frontend/src/components/Tabs/Tabs.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tabs">
+    <div v-if="title" class="tabs__title">{{ title }}</div>
+    <div class="tabs__items">
+      <div
+        :class="tab === activeTab ? `tabs__item_active` : null"
+        class="tabs__item"
+        v-for="(tab, i) in tabs"
+        :key="i"
+        @click="setActiveTab(tab)"
+      >
+        {{ tab }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./Tabs";
+</style>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    activeTab: {
+      type: String,
+      default: "",
+    },
+    tabs: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  components: {},
+  data: () => {
+    return {};
+  },
+  methods: {
+    setActiveTab(tab) {
+      this.$emit("update:activeTab", tab);
+    },
+  },
+};
+</script>
--- frontend/src/components/Widgets/Transfers/TransfersHeader/TransfersHeader.vue
+++ frontend/src/components/Widgets/Transfers/TransfersHeader/TransfersHeader.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="transfers-header">
+    <Tabs title="Status" :tabs="statuses" :active-tab.sync="activeStatus" />
     <Input
       :value="searchQuery"
       icon="SearchIcon"
@@ -14,12 +15,15 @@
 
 <script>
 import Input from "@/components/Controls/Input/Input";
+import Tabs from "@/components/Tabs/Tabs";
 
 export default {
-  components: { Input },
+  components: { Input, Tabs },
   data: () => {
     return {
       searchQuery: "",
+      statuses: ["All", "Downloading", "Seeding", "Completed", "Paused"],
+      activeStatus: "All",
     };
   },
 };
